3cf114948f3b5dcd9ea1f573ae1554b080e006cb
Rubens Diego <rdsdo2011@gmail.com>
2020-11-11 23:16:16 -0300

    criando inicio banco

---
+++ src/database/migration/1605147321326-Salario.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Salario1605147321326 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
