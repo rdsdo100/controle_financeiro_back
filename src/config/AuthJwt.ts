671882ad9928929b6e89b3e0602c58d8c4991b18
Rubens Deigo <rdsdo2011@gmail.com>
2021-06-25 21:23:41 -0300

    Refatorando autentificação

--- src/config/AuthJwt.ts
+++ src/config/AuthJwt.ts
@@ -1,24 +1,17 @@
-import { NextFunction, Request, Response } from "express";
-import jwt from 'jsonwebtoken'
-import { Usuarios } from "../entity/Usuarios";
+import jwt from 'jsonwebtoken';
+import { Usuarios } from '../entity/Usuarios';
 
 export default class AuthJwt {
-
-
-
-  async assinar(usuario: Usuarios) {
-
-    if (usuario.ativo && !usuario.bloqueado) {
-        const token = jwt.sign(
-            { id: usuario.id, nomeUsuario: usuario.nomeUsuario },
-            String(process.env.JWT_TOKEN),
-            { expiresIn: '10d' })
-        return token
-
-    } else {
-        return "Usuario Incorreto!"
+    async assinar(usuario: Usuarios) {
+        if (usuario.ativo) {
+            const token = jwt.sign(
+                { id: usuario.id, nomeUsuario: usuario.nomeUsuario, nomeAdministrador: usuario.administrador },
+                String(process.env.JWT_TOKEN),
+                { expiresIn: '10d' },
+            );
+            return token;
+        } else {
+            return 'Usuario Incorreto!';
+        }
     }
-}
-
- 
 }
