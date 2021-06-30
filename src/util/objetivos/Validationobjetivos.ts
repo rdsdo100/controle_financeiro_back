d41f999245b7f454c5a3b80809e0f80b3a6762b2
Rubens Deigo <rdsdo2011@gmail.com>
2021-06-29 22:29:02 -0300

    refatorando

--- src/util/objetivos/Validationobjetivos.ts
+++ src/util/objetivos/Validationobjetivos.ts
@@ -1,8 +1,8 @@
-import { isAfter, isValid } from 'date-fns';
+import { isAfter } from 'date-fns';
 import { ObjetivosFinaceiros } from '../../entity/ObjetivosFinaceiros';
 
 interface IResponseObjetivos {
-    isValid: boolean;
+    isValidated: boolean;
     message?: string;
     status?: number;
 }
@@ -11,20 +11,20 @@ export default class Validationobjetivos {
     isObjetivos(objetivos: ObjetivosFinaceiros): IResponseObjetivos {
         if (!isAfter(objetivos.dataPrevistaObjetivos, Date.now())) {
             return {
-                isValid: false,
+                isValidated: false,
                 message: 'Os objetivos deve ser de no momimo um dia!',
                 status: 400,
             };
         }
         if (objetivos.valorObjetivos <= 0) {
             return {
-                isValid: false,
+                isValidated: false,
                 message: 'Os valorObjetivos não pode ser 0 ou valor negatvo!',
                 status: 400,
             };
         }
         return {
-            isValid: true,
+            isValidated: true,
         };
     }
 }
