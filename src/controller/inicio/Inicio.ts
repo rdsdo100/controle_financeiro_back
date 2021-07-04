3a5c006cb2d369570f510d5217234df447a6469e
Rubens Deigo <rdsdo2011@gmail.com>
2021-07-04 15:31:49 -0300

    refatorando

--- src/controller/inicio/Inicio.ts
+++ src/controller/inicio/Inicio.ts
@@ -5,7 +5,7 @@ import {Request, Response} from "express";
 export default class Inicio {
 
     inicio (req: Request , res: Response) {
-        res.send({ok : 'Online' , Name: 'Academia'})
+        res.send({ok : 'Online' , Name: 'Controle finaceiro'})
     }
 
 }
